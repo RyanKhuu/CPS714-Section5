204cbd6c8edf548125888136a9e2d9b7f79b89d4
Ryan Khuu <ryankhuu1@gmail.com>
2025-11-17 18:31:00 -0500

    CPS-714: Base commit for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
+
+// ==== Admin Dashboard ====
+app.get("/", (request, response) => {
+    response.send('Hello World!');
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
